e990305f1bb04556df3926e3a0448052855a9d64
Shopping <xpzheng6@163.com>
2023-03-08 15:35:55 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/** @type {import('@types/eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    '@shoppingzh', //
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {},
+}
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import('@types/prettier').Config}*/
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: Number.MAX_SAFE_INTEGER,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  bracketSameLine: false,
+}
